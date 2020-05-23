ed140386d5cd58afba91d9de7c24de2972cbca68
root <jaldesigner@uol.com.br>
2020-05-23 15:05:59 -0300

    Alteraçoes sem erros

--- insertAcompanhamentoPratoDia.php
+++ insertAcompanhamentoPratoDia.php
@@ -15,13 +15,18 @@ $ID_APP = $obj['ID_APP'];
 $data = date('d-n-Y');
 $exibe = 1;
 
-if($in_valor != ''){
-    //$con->query("UPDATE opcao_prato_dia SET exibir='$exibe' WHERE id_App='$ID_APP' AND nome_prato='$in_valor'");
-    $con->query("INSERT INTO opcao_prato_dia (id_app, nome_opcao, data_opcao_prato_dia)VALUES('$ID_APP', '$in_valor', '$data')");
+$sql = "INSERT INTO opcao_prato_dia (id_app, nome_opcao, exibir, data_opcao_prato_dia)VALUES('$ID_APP', '$in_valor','$exibe', '$data')";
 
-    $MSG[0] = 'gravado';
-    $MSG[1] = '1';
-    $json = json_encode($MSG);
+if ($in_valor != '') {
+    //$con->query("UPDATE opcao_prato_dia SET exibir='$exibe' WHERE id_App='$ID_APP' AND nome_prato='$in_valor'");
+    if ($con->query($sql)) {
+        $MSG[0] = 'gravado';
+        $MSG[1] = '1';
+        $json = json_encode($MSG);
+    }else{
+        $MSG[0] = $con->error;
+        $json = json_encode($MSG);
+    }
 }
 
 echo $json;
--- listaAcompanhamento.php
+++ listaAcompanhamento.php
@@ -1,59 +1,23 @@
 <?php
-/**
- * INclusão do documento de configuração do banco de dados
- */
+
 include './dbconfig.php';
-
-/**
- * Criamos a variável com a entrada do json 
- * atravéz da função file_get_contents()
- */
 $json = file_get_contents('php://input');
-
-/**
- * Transformamos em objeto o json que capituramos na entrada
- * com a função json_decoder()
- */
-$obj = json_decode($json, true);
-
-/**
- * Colocamos em variáveis os objetos que decodificamos
- * e colocamos na variável $obj
- */
+$obj  = json_decode($json, true);
 //$ID_APP = 'SUQ6IDEKRW1wcmVzYTogSkQgUmVmZWnDp8O1ZXMKUmVwcmVzZW50YW50ZTogSm9uYXMgQWx2ZXMgTHVjYXM=';
 $ID_APP = $obj['ID_APP'];
 //$data = date('d-n-Y'); //$obj['data'];
 
-/**
- * selecionamos todas as opções que há no banco de dados
- * e colocamos na variável $sql
- */
 $sql = "SELECT * FROM opcao_prato WHERE id_App = '$ID_APP'";
 
-/**
- * Aqui faremos a query (corrida), por todo o banco
- * e pegamos todos os resultados 
- */
- $result = $con->query($sql);
+$result = $con->query($sql);
 
- /**
-  * Contaremos o resultado para saber quantos foram achados
-  * e se o resultado for maior que zero
-  * fazemos um loop com while e salvamos em um array tudo que for retornado
-  * na função fetch_assoc() e dentro do while retornamos já colocando nesse
-  * loop um json_encode() para retornamos para o App
- */
-  if($result->num_rows > 0){
-      while($row[] = $result->fetch_assoc()){
-          $opcao = $row;
-          $json = json_encode($opcao);
-      }
-  }
+if ($result->num_rows > 0) {
+    while ($row[] = $result->fetch_assoc()) {
+        $opcao = $row;
+        $json = json_encode($opcao);
+    }
+}
 
-  /**
-   * Aqui damos um echo
-   * para ser exibido o resultado do json e
-   */
-  echo $json;
+echo $json;
 
-  $con->close(); //Fechamos a conexão
+$con->close(); //Fechamos a conexão
